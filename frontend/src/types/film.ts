f83d2c174cb4b5cf4c1a03960283fc21e2bfdb1d
ThomasOttosson <tomasottosson1@gmail.com>
2026-06-30 17:57:05 +0200

    Create frontend architecture and project structure

---
+++ frontend/src/types/film.ts
@@ -0,0 +1,16 @@
+export interface Scene {
+    id: number;
+    title: string;
+    narration: string;
+    mood: string;
+    duration: string;
+}
+
+export interface FilmProject {
+    id: string;
+    title: string;
+    description: string;
+    genre: string;
+    style: string;
+    scenes: Scene[];
+}
